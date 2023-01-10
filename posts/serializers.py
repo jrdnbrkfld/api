94d4d28a5be376647e3bd7a10a52ac85bc7b801f
Jordan <jrdnbrkfld@icloud.com>
2023-01-10 11:37:42 +0000

    added category to Meta class

--- posts/serializers.py
+++ posts/serializers.py
@@ -44,5 +44,5 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'owner', 'is_owner', 'profile_id', 'profile_image',
                   'created_at', 'updated_at', 'title', 'content',
-                  'image', 'image_filter', 'like_id',
-                  'likes_count', 'comments_count']
+                  'image', 'like_id',
+                  'likes_count', 'comments_count', 'category']
